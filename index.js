6ec0f70e4d15763ada2adc3567ae03967206f0e7
Pulkit0111 <ptyagi1551@gmail.com>
2024-02-14 12:27:02 +0530

    added the cors middleware

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express=require("express")
 const {connection}=require("./config/db")
 const {userRouter}=require("./routes/user.routes")
 const {noteRouter}=require("./routes/note.routes")
+const cors=require("cors")
 require("dotenv").config()
 
 const app=express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/users",userRouter)
 app.use("/notes",noteRouter)
